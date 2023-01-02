fee33ecb30a29564b5ec13b7a8b21f1cc166614c
manihateu <nik852.nikita@mail.ru>
2023-01-03 03:26:01 +0400

    _

---
+++ build/precache-manifest.41bec0b50792f27b4a9ea67d600efc76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1934ffdc35c12427313c5965b7140efd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e0e0f196e0bca8af66b",
+    "url": "/static/css/main.e538b6ad.chunk.css"
+  },
+  {
+    "revision": "b48e148b94264bc86a3f",
+    "url": "/static/js/2.114c604f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.114c604f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e0e0f196e0bca8af66b",
+    "url": "/static/js/main.3058b5e1.chunk.js"
+  },
+  {
+    "revision": "8265d98f947ffef73b76",
+    "url": "/static/js/runtime-main.c174d19a.js"
+  }
+]);
